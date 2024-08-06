f5ceb70bea9f6d4eabf1b53ccc4b4c7743fa449b
Ramiz Raja <informramiz@gmail.com>
2024-08-07 01:46:16 +0500

    feat: Add scaffold helper components

--- app/src/main/java/io/portalhq/portalhackathon/ui/screens/HomeScreen.kt
+++ app/src/main/java/io/portalhq/portalhackathon/ui/screens/HomeScreen.kt
@@ -1,13 +1,25 @@
 package io.portalhq.portalhackathon.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 import io.portalhq.portalhackathon.presentation.home.HomeViewModel
+import io.portalhq.portalhackathon.presentation.home.HomeViewState
+import io.portalhq.portalhackathon.ui.components.AppScaffold
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
     val viewState = viewModel.viewState.collectAsState()
-    Text(text = "Home Screen - Hello World with address: ${viewState.value.solanaAddress}")
+    AppScaffold(title = "Portal Hackathon") {
+        HomeScreenUI(viewModel = viewModel, viewState = viewState.value)
+    }
+}
+
+@Composable
+private fun HomeScreenUI(viewModel: HomeViewModel, viewState: HomeViewState) {
+    Column {
+        Text(text = "Home Screen - Hello World with address: ${viewState.solanaAddress}")
+    }
 }
