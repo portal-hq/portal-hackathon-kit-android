58b4ba4599d061b9c9c8ebc17f35bc76e6c75bd4
Ramiz Raja <informramiz@gmail.com>
2024-08-08 03:37:21 +0500

    chore: Add Portal custodian API

--- app/src/main/java/io/portalhq/portalhackathon/core/commonconstants/PortalConstants.kt
+++ app/src/main/java/io/portalhq/portalhackathon/core/commonconstants/PortalConstants.kt
@@ -1,7 +1,13 @@
 package io.portalhq.portalhackathon.core.commonconstants
 
 object PortalConstants {
-    const val PORTAL_CLIENT_API_KEY = "YOUR_API_KEY"
+    const val PORTAL_CLIENT_API_KEY = "YOUR API KEY"
+    const val PORTAL_CLIENT_ID = "YOUR_CLIENT_ID"
+
+    // Portal Solana API URLs
     const val PORTAL_SOLANA_DEV_NET_API_URL = "https://api.portalhq.io/api/v3/clients/me/chains/solana-devnet/"
     const val PORTAL_SOLANA_MAIN_NET_API_URL = "https://api.portalhq.io/api/v3/clients/me/chains/solana/"
+
+    // Portal Custodian API URLs
+    const val PORTAL_CUSTODIAN_API_URL = "https://portalex-mpc.portalhq.io/mobile/"
 }
